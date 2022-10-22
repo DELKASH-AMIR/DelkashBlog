1591611973d9f8538e18cfe37c7be5a171372163
DELKASH-AMIR <94930805+DELKASH-AMIR@users.noreply.github.com>
2022-10-22 12:37:17 +0330

    Update urls.py

--- FirstBlog/Blog/urls.py
+++ FirstBlog/Blog/urls.py
@@ -1,7 +1,7 @@
-from django import views
 from django.urls import path
 from . import veiws
 
 urlpatterns = [
     path('',views.post_list,name='post_list'),
+    path('post/<int:pk>',views.post_detail,name='post_detail')
 ]
